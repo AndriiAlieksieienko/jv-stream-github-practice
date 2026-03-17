afab30eacd55eacbb959e037e2e4f9697c6a006b
Andrii Alieksieienko <andriy.alieksieienko@gmail.com>
2026-03-17 12:20:11 +0200

    resolved the task

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int REQUIRED_YEARS = 35;
+    private static final int REQUIRED_LIVING_YEARS = 10;
+    private static final int FROM_YEAR_INDEX = 0;
+    private static final int TO_YEAR_INDEX = 1;
+
     //write your code here
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int fromYear = Integer.parseInt(years[FROM_YEAR_INDEX]);
+        int toYear = Integer.parseInt(years[TO_YEAR_INDEX]);
+
+        return candidate.getAge() >= REQUIRED_YEARS
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && toYear - fromYear >= REQUIRED_LIVING_YEARS;
+    }
 }
